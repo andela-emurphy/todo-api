458e6d61a06f5a88afa2e69eb657a48f9d6e6e4d
enaho murphy <murphy.enaho@andela.com>
2016-11-27 18:58:39 +0100

    add todo and todo id endpoints

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const app = require('express')(),
-      port = process.env.PORT || 8000
+      port = process.env.PORT || 2000;
+
+let todos = [{
+  id: 1,
+  description: "meet mom for launch",
+  completed: false,
+}, {
+  id: 2,
+  description: "go to market",
+  completed: false
+}]
 
 app.get('/', (req, res) => {
   res.send('</h1>Hello world</h1>');
+})
+
+app.get('/todos', (req, res) => {
+  res.status(200).json(todos);
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  let found;
+  todos.forEach(todo => {
+    if(todo.id === id) {
+      found  = todo;
+      return true;
+    }
+  })
+  if(found) {
+    return res.status(200).json(found);
+  } else {
+    return res.status(404).json({success :false, message: 'todo with that id not found'});
+  }
 })
 
 
