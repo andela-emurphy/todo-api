c0529cadf7541f36908fe3a89c64a1dcdf207b77
enaho murphy <murphy.enaho@andela.com>
2016-11-27 20:47:17 +0100

    add post to TODO

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const app = require('express')(),
-      port = process.env.PORT || 2000;
+  bodyParser = require('body-parser'),
+  port = process.env.PORT || 2000;
 
-let todos = [{
-  id: 1,
-  description: "meet mom for launch",
-  completed: false,
-}, {
-  id: 2,
-  description: "go to market",
-  completed: false
-}]
+let todos = [],
+  todoNextId = 1;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}))
 
 app.get('/', (req, res) => {
   res.send('</h1>Hello world</h1>');
@@ -23,18 +19,36 @@ app.get('/todos/:id', (req, res) => {
   const id = parseInt(req.params.id);
   let found;
   todos.forEach(todo => {
-    if(todo.id === id) {
-      found  = todo;
+    if (todo.id === id) {
+      found = todo;
       return true;
     }
   })
-  if(found) {
+  if (found) {
     return res.status(200).json(found);
   } else {
-    return res.status(404).json({success :false, message: 'todo with that id not found'});
+    return res.status(404).json({
+      success: false,
+      message: 'todo with that id not found'
+    });
   }
 })
 
+app.post('/todos', (req, res) => {
+  if (req.body.description){
+     let todo = {
+      id: todoNextId,
+      description: req.body.description,
+      completed: req.body.completed  || false
+    };
+    todos.push(todo);
+    todoNextId++;
+    res.status(201).json({success:true, message: "todo added"});
+  }else{
+    res.status('400').json({success:false, message:"please add a description"});
+  }
+   
+})
 
 app.listen(port, () => {
   console.log('express is listening on port ' + port);
