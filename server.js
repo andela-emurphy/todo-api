46995888106f63da85150d42b1d346583938397e
enaho murphy <murphy.enaho@andela.com>
2016-11-28 20:47:40 +0100

    add update TODO route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require('express')(),
 let todos = [],
   todoNextId = 1;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false}))
+// app.use(bodyParser.urlencoded({ extended : true }))
 
 app.get('/', (req, res) => {
   res.send('</h1>Hello world</h1>');
@@ -26,16 +26,17 @@ app.get('/todos/:id', (req, res) => {
       success: false,
       message: 'todo with that id not found'
     });
-  }
+  };
 })
 
 app.post('/todos', (req, res) => {
   let body = _.pick(req.body, 'description', 'completed');
-  if (body.description && body.description.trim().length !== 0) {
+  console.log(body);
+  if (body.description && body.description.trim().length !== 0 && _.isBoolean(body.completed)) {
     let todo  = {
       id : todoNextId,
       description: body.description,
-      completed: body.completed ? true : false
+      completed: body.completed
     };
     todos.push(todo);
     todoNextId++;
@@ -58,6 +59,28 @@ app.delete('/todos/:id', (req, res) => {
 })
 
 
+app.put('/todos/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let body = _.pick(req.body, 'completed', 'description');
+  let matchedTodo = _.findWhere(todos, {id: id })
+  let validAttributes = {}
+
+  if(!matchedTodo) {
+    return res.status(404).json()
+  }
+  if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  }else if(body.hasOwnProperty(completed)) {
+    return res.status(400).json();
+  }
+  if(_.isString(body.description) && body.description.trim() > 0) {
+    validAttributes.description = body.description;
+  }else if(body.hasOwnProperty('description')) {
+    return res.status(400).json()
+  }
+  _.extend(matchedTodo, validAttributes);
+  res.status(200).json(validAttributes);
+});
 
 app.listen(port, () => {
   console.log('express is listening on port ' + port);
